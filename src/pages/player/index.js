c03d8e0dbcddd974e1eb5c6c7df4fde35456b26b
José Muñoz Ruiz <jozeepp@gmail.com>
2020-04-01 00:05:00 -0300

    se implementa service worker en mediaPlayer para cachear información

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -10,3 +10,9 @@ const media = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPau
 
 buttonPlay.onclick = () => media.togglePlay();
 buttonMute.onclick = () => media.toggleMute();
+
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .catch(error =>  console.log(error.message))
+}
