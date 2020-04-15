91e781d0918f9e01ef1d80ab355dda948def1791
José Muñoz Ruiz <jozeepp@gmail.com>
2020-04-14 23:10:17 -0400

    estilo ads

--- src/plugins/Ads/index.ts
+++ src/plugins/Ads/index.ts
@@ -6,13 +6,17 @@ class AdsPlugins {
   private player: MediaPlayer;
   private media: HTMLMediaElement;
   private currentAd: Ad;
+  private adsContainer: HTMLElement;
 
   constructor() {
     this.ads = Ads.getIstance();
+    this.adsContainer = document.createElement('div');
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
   }
+
   run(player: MediaPlayer) {
     this.player = player;
+    this.player.container.appendChild(this.adsContainer);
     this.media = this.player.media;
     this.media.addEventListener('timeupdate', this.handleTimeUpdate);
   }
@@ -30,7 +34,21 @@ class AdsPlugins {
     }
     const ad = this.ads.getAds();
     this.currentAd = ad;
-    console.log(ad);
+    this.adsContainer.innerHTML = `
+      <div class="ads">
+        <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+          <img class="ads__img" src="${this.currentAd.imageUrl}" />
+          <div class="ads__info">
+            <h5 class="ads__title">${this.currentAd.title}</h5>
+            <p class="ads__body">${this.currentAd.body}</p>
+          </div>
+        </a>
+      </div>
+    `;
+    setTimeout(() => {
+      this.currentAd = null;
+      this.adsContainer.innerHTML = '';
+    }, 10000);
   }
 }
 
