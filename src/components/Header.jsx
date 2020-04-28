f7c46788085a8192b0480302b82a02f1e9e5625e
José Muñoz Ruiz <jozeepp@gmail.com>
2020-04-28 07:09:11 -0400

    Header component React

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Header = () => (
+  <header className='header'>
+    <div className='header__logo'>
+      <img className='header__img' src='../../../assets/play-icon.png' alt='Video Platform' />
+      <h3>
+        Video
+        <strong>Platform</strong>
+      </h3>
+    </div>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src='../../assets/user-icon.png' alt='' />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li>
+          <a href='/components/login/login.html'>Login</a>
+        </li>
+        <li>
+          <a href='/buscador.html'>Buscador</a>
+        </li>
+        <li>
+          <a href='/'>Cuenta</a>
+        </li>
+        <li>
+          <a href='/'>Cerrar Sesión</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
--- src/components/HelloWord.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-
-const HelloWord = () => {
-  return <div>Hello Word!!!</div>;
-};
-
-export default HelloWord;
