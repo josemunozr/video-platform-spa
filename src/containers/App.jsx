2b7bb0fee735a92ac93c94952996928ad9d20ba5
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-01 23:47:10 -0400

    use useState and useEffect for fetch data

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,31 +7,51 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Mis Lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Otros'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: []
+  });
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {videos.mylist.length !== 0 && (
+        <Categories title='Mis Lista'>
+          <Carousel>
+            {videos.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.trends.length !== 0 && (
+        <Categories title='Tendencias'>
+          <Carousel>
+            {videos.trends.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.originals.length !== 0 && (
+        <Categories title='Otros'>
+          <Carousel>
+            {videos.originals.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
 
-    <Footer />
-  </div>
-);
-
+      <Footer />
+    </div>
+  );
+};
 export default App;
