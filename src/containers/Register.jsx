0f08bd131a37b21322f5334a0835c4c6df2c516c
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-06 07:32:14 -0400

    add register component

---
+++ src/containers/Register.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../assets/styles/components/Register.scss';
+
+const Register = () => (
+  <section className='registro'>
+    <section className='registro__container'>
+      <h2>Regístrate</h2>
+      <form className='registro__container--form'>
+        <input className='input' type='text' placeholder='Nombre' />
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <button type='submit' className='button'>
+          Registrarme
+        </button>
+      </form>
+      <p className='registro__container--register'>
+        <a href='../login/login.html'>Iniciar sesión</a>
+      </p>
+    </section>
+  </section>
+);
+
+export default Register;
