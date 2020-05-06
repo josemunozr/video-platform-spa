a329963711007f2c2ee87848f2835a60d7a2161a
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-06 07:42:47 -0400

    add NotFound component for others routes

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <section className='not-found'>
+    <h1 className='animated pulse'>404</h1>
+    <p>Página no encontrada</p>
+  </section>
+);
+
+export default NotFound;
