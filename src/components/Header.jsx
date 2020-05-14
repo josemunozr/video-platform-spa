0915f6e69fac08250caee5aa224cffd7a13a6689
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-14 07:58:50 -0400

    add gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import playIcon from '../assets/statics/play-icon.png';
 import userIcon from '../assets/statics/user-icon.png';
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <div className='header__logo'>
-        <img className='header__img' src={playIcon} alt='Video Platform' />
-        <h3>
-          Video
-          <strong>Platform</strong>
-        </h3>
-      </div>
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <a href='/'>Cuenta</a>
-        </li>
-        <Link to='/login'>Login</Link>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const { user } = props;
 
-export default Header;
+  const hasUser = Object.keys(user).length > 0;
+
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <div className='header__logo'>
+          <img className='header__img' src={playIcon} alt='Video Platform' />
+          <h3>
+            Video
+            <strong>Platform</strong>
+          </h3>
+        </div>
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          {hasUser ? <img src={gravatar(user.email)} alt={user.email} /> : <img src={userIcon} alt='' />}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href='/'>Cuenta</a>
+          </li>
+          <Link to='/login'>Login</Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps, null)(Header);
