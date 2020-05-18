c55e2541160b7edb114c208a11dc095c9a9ac9ea
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-18 08:22:56 -0400

    playing video in player component

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import NotFound from './NotFound';
 import '../assets/styles/components/Player.scss';
 
 const Player = (props) => {
-  const handleGoToBack = () => {
-    props.history.push('/');
+  const { id } = props.match.params;
+  const { playing } = props;
+  const hasPlaying = Object.keys(playing).length > 0;
+
+  const handleGoBack = () => {
+    props.history.goBack();
   };
-  return (
+
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
-        <button type='button' onClick={handleGoToBack}>
+        <button type='button' onClick={handleGoBack}>
           Regresar
         </button>
       </div>
     </div>
+  ) : (
+    <NotFound />
   );
 };
 
-export default Player;
+const mapStateToProps = (state) => ({
+  playing: state.playing
+});
+
+const mapDistpatchToProps = {
+  getVideoSource
+};
+
+export default connect(mapStateToProps, mapDistpatchToProps)(Player);
