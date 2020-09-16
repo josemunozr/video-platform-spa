204884c7b84e86776cd3e06b653b88b66a2b9637
José Muñoz Ruiz <jozeepp@gmail.com>
2020-09-16 08:17:21 -0300

    Abstracting React Router, creating history and making initialState more accessible

--- src/client/index.js
+++ src/client/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routers/App';
+import InitialState from './utils/initialState';
 
-import InitialState from '../../initialState.json';
-
+const history = createBrowserHistory();
 const store = createStore(reducer, InitialState);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
