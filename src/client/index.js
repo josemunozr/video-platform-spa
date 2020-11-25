ad7c4c879573f26f0a44d05c2ea0791a1de64ba0
José Muñoz Ruiz <jozeepp@gmail.com>
2020-11-24 23:18:10 -0300

    redux-thunk configuration

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routers/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const history = createBrowserHistory();
 const preloadedState = window.__PRELOADED_STATE__;
-const store = createStore(reducer, preloadedState);
+const store = createStore(
+  reducer,
+  preloadedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
 delete window.__PRELOADED_STATE__;
 
 ReactDOM.hydrate(
